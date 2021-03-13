e4c1635f4da90c44cf902d4738cbe4edf23d34d1
TruongTD1112 <truong.td173429@sis.hust.edu.vn>
2021-03-13 22:53:43 +0700

    upload assets

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int number = 1;
+      Console.WriteLine("gia tri cua so la " + number);
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}
